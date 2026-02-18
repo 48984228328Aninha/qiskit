258a0b7e9be18de8e61b6d67b755268e4898a197
48984228328Aninha <Amanda_rodrigues.de.siqueira@hotmail.com>
2026-02-18 20:48:48 -0300

    chore: add qubits measure

--- qisit.py
+++ qisit.py
@@ -6,12 +6,19 @@ qc.qubits
 
 qr1 = QuantumRegister(2, "qreq1")
 qr2 = QuantumRegister(1, "qreq2")
-qr3 = QuantumRegister(3, "creg1")
+qr3 = ClassicalRegister(3, "creg1")
 
 combined_circ = QuantumCircuit(qr1, qr2, qr3)
 
 combined_circ.qubits
 
+combined_circ.h(qr1)
+
+combined_circ.measure(qr1[0], qr3[0])
+combined_circ.measure(qr1[1], qr3[1])
+combined_circ.measure(qr2[0], qr3[2])
+
+print(combined_circ.draw)
 
 desired_qubit = qr2[0]
 print("Index:", combined_circ.find_bit(desired_qubit).index)
